55b83ebf3800820424410df1d7112e4f3a7da0ad
embydextrous <arjit.agarwal.1000@gmail.com>
2017-06-01 07:51:41 +0530

    Initial Commit

---
+++ app/src/main/java/books/doread/android/DoReadActivity.kt
@@ -0,0 +1,12 @@
+package books.doread.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DoReadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_do_read)
+    }
+}
